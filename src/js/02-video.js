b28d519f830969d120d336b16b5c44100ffdf3bf
VolodymyrBodniak <bodnjak@gmail.com>
2023-07-13 22:12:59 +0400

    Update hw-08-task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo('vimeo-player');
+
+player.getCurrentTime().then(time => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  } else {
+    localStorage.setItem('videoplayer-current-time', time);
+  }
+});
+
+player.on('timeupdate', throttle(updateCurrentTime, 1000));
+
+function updateCurrentTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
