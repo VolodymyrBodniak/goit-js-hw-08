9c15c4df265cb877ee35418c7c8bad1a1e0a27ea
VolodymyrBodniak <bodnjak@gmail.com>
2023-07-14 08:28:39 +0400

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+form.addEventListener('input', throttle(saveFormState, 500));
+
+loadFormState();
+
+form.addEventListener('submit', handleSubmit);
+
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}
+
+function loadFormState() {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.removeItem('feedback-form-state');
+
+  emailInput.value = '';
+  messageInput.value = '';
+
+  console.log(formState);
+}
