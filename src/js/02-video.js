ba994cfac1a67588c88710d02372a0a2683587ef
VolodymyrBodniak <bodnjak@gmail.com>
2023-07-20 21:52:31 +0400

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,39 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Vimeo('vimeo-player');
+// const player = new Vimeo('vimeo-player');
 
-player.getCurrentTime().then(time => {
-  const currentTime = localStorage.getItem('videoplayer-current-time');
-  if (currentTime) {
-    player.setCurrentTime(currentTime);
-  } else {
-    localStorage.setItem('videoplayer-current-time', time);
-  }
-});
+// player.getCurrentTime().then(time => {
+//   const currentTime = localStorage.getItem('videoplayer-current-time');
+//   if (currentTime) {
+//     player.setCurrentTime(currentTime);
+//   } else {
+//     localStorage.setItem('videoplayer-current-time', time);
+//   }
+// });
 
-player.on('timeupdate', throttle(updateCurrentTime, 1000));
+// player.on('timeupdate', throttle(updateCurrentTime, 1000));
 
-function updateCurrentTime({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+// function updateCurrentTime({ seconds }) {
+//   localStorage.setItem('videoplayer-current-time', seconds);
+// }
+
+const vimeoPlayerRef = document.querySelector('iframe');
+const player = new Vimeo(vimeoPlayerRef);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
 }
+
+function setTime() {
+  const time = localStorage.getItem(STORAGE_KEY);
+  if (time) {
+    player.setCurrentTime(time);
+  }
+}
+
+setTime();
